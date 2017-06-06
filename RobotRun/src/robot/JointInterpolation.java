214eba4238b658570b40c0ab8bcebf8c6db499e9
Joshua Hooker <jbhooker@mtu.edu>
2017-06-06 12:30:23 -0400

    Working on Robot motion

---
+++ RobotRun/src/robot/JointInterpolation.java
@@ -0,0 +1,146 @@
+package robot;
+
+import global.RMath;
+import processing.core.PApplet;
+import processing.core.PConstants;
+
+/**
+ * Defines the robot's target joint motion, which uses rotational interpolation
+ * to move from the robot's current joint angles to the target angles.
+ * 
+ * @author Joshua Hooker
+ */
+public class JointInterpolation extends JointMotion {
+	
+	/**
+	 * The joint angles, to which to move.
+	 */
+	private final float[] TGT_ANGLES;
+	
+	/**
+	 * Sets the target joint angles and joint motion directions for the
+	 * rotational interpolation. In addition, the speed modifiers for each of
+	 * the roobt's segments is updated in such a way, that each joint will
+	 * finish interpolation at the same time.
+	 * 
+	 * @param robot		The robot. for which this motion is defined
+	 * @param tgtAngles	The target joint angles, to which to interpolate
+	 */
+	public JointInterpolation(RoboticArm robot, float[] tgtAngles) {
+		TGT_ANGLES = new float[6];
+		setupRotationalInterpolation(robot, tgtAngles);
+	}
+	
+	@Override
+	public int executeMotion(RoboticArm robot) {
+		// Count how many joints are still in motion
+		int ret = 0;
+		float speed = robot.getLiveSpeed() / 100f;
+		
+		for(int jdx = 0; jdx < 6; ++jdx) {
+			RSegWithJoint seg = robot.getSegment(jdx);
+			
+			if (JOINT_MOTION[jdx] != 0) {
+				float distToDest = PApplet.abs(seg.getJointRotation()
+						- TGT_ANGLES[jdx]);
+
+				if (distToDest >= (seg.getSpeedModifier() * speed)) {
+					++ret;
+					/* Move the joint based on the roobt's liveSpeed, the
+					 * direction of motion and the segment's speed modifier */
+					float newRotation = RMath.mod2PI(seg.getJointRotation()
+							+ JOINT_MOTION[jdx] * seg.getSpeedModifier()
+							* speed);
+					
+					seg.setJointRotation(newRotation);
+
+				} else if (distToDest > 0.00009f) {
+					// Destination too close to move at current speed
+					seg.setJointRotation(TGT_ANGLES[jdx]);
+				}
+			}
+		}
+		
+		return ret;
+	}
+	
+	/**
+	 * Sets the target joint angles and joint motion directions for the
+	 * rotational interpolation. In addition, the speed modifiers for each of
+	 * the roobt's segments is updated in such a way, that each joint will
+	 * finish interpolation at the same time.
+	 * 
+	 * @param robot		The robot. for which this motion is defined
+	 * @param tgtAngles	The target joint angles, to which to interpolate
+	 */
+	public void setupRotationalInterpolation(RoboticArm robot, float[] tgtAngles) {
+		float[] minDist = new float[6];
+		float maxMinDist = Float.MIN_VALUE;
+		
+		for (int jdx = 0; jdx < 6; ++jdx) {
+			// Set the target angle for the joint index
+			TGT_ANGLES[jdx] = RMath.mod2PI(tgtAngles[jdx]);
+			// Calculate the minimum distance to the target angle
+			minDist[jdx] = RMath.minDist(robot.getSegment(jdx).getJointRotation(),
+					TGT_ANGLES[jdx]);
+			
+			if (Math.abs(minDist[jdx]) > maxMinDist) {
+				/* Update the maximum distance necessary for one of the joints
+				 * to reach its target angle. */
+				maxMinDist = Math.abs(minDist[jdx]);
+			}
+		}
+		
+		for(int jdx = 0; jdx < 6; jdx++) {
+			RSegWithJoint seg = robot.getSegment(jdx);
+			/* Update the speed modifier for the joint based off the ratio
+			 * between the distance necessary for this joint to travel and that
+			 * of the joint with the longest distance to travel */
+			seg.setSpdMod(Math.abs(minDist[jdx]) / maxMinDist * (PConstants.PI / 60f));
+			
+			// Check joint motion range
+			if (seg.LOW_BOUND == 0f && seg.UP_BOUND == PConstants.TWO_PI) {
+				JOINT_MOTION[jdx] = (minDist[jdx] < 0) ? -1 : 1;
+				
+			} else {  
+				/* Determine if at least one bound lies within the range of the
+				 * shortest angle between the current joint angle and the
+				 * target angle. If so, then take the longer angle, otherwise
+				 * choose the shortest angle path. */
+
+				/* The minimum distance from the current joint angle to the
+				 * lower bound of the joint's range */
+				float dist_lb = RMath.minDist(seg.getJointRotation(),
+						seg.LOW_BOUND);
+
+				/* The minimum distance from the current joint angle to the
+				 * upper bound of the joint's range */
+				float dist_ub = RMath.minDist(seg.getJointRotation(),
+						seg.UP_BOUND);
+
+				if (minDist[jdx] < 0) {
+					if( (dist_lb < 0 && dist_lb > minDist[jdx]) ||
+							(dist_ub < 0 && dist_ub > minDist[jdx]) ) {
+						
+						// One or both bounds lie within the shortest path
+						JOINT_MOTION[jdx] = 1;
+						
+					} else {
+						JOINT_MOTION[jdx] = -1;
+					}
+					
+				} else if(minDist[jdx] > 0) {
+					if( (dist_lb > 0 && dist_lb < minDist[jdx]) ||
+							(dist_ub > 0 && dist_ub < minDist[jdx]) ) {
+						
+						// One or both bounds lie within the shortest path
+						JOINT_MOTION[jdx] = -1;
+						
+					} else {
+						JOINT_MOTION[jdx] = 1;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ RobotRun/src/robot/JointJog.java
@@ -0,0 +1,72 @@
+package robot;
+
+import global.Fields;
+import global.RMath;
+
+/**
+ * Defines the robot's jog motion in the joint coordinate frame.
+ * 
+ * @author Joshua Hooker
+ */
+public class JointJog extends JointMotion {
+	
+	/**
+	 * Initializes the all joint motion to zeros.
+	 */
+	public JointJog() {
+		super();
+	}
+	
+	@Override
+	public int executeMotion(RoboticArm robot) {
+		for(int jdx = 0; jdx < 6; jdx += 1) {
+			RSegWithJoint seg = robot.getSegment(jdx);
+			
+			// Is the joint in motion?
+			if (JOINT_MOTION[jdx] != 0) {
+				/* Move the joint based on the roobt's liveSpeed, the direction
+				 * of motion and the segment's speed modifier */
+				float delta = JOINT_MOTION[jdx] * seg.getSpeedModifier() *
+						robot.getLiveSpeed() / 100f;
+				float trialAngle = RMath.mod2PI(seg.getJointRotation() + delta);
+				
+				if(!seg.setJointRotation(trialAngle)) {
+					// Invalid joint rotation
+					Fields.debug("A[%d]: %f\n", jdx, trialAngle);
+					JOINT_MOTION[jdx] = 0;
+					// TODO REFACTOR THIS
+					// RobotRun.getInstance().updateRobotJogMotion(i, 0);
+				}
+			}
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * Creates a copy of the directions of joint motion defined by this object.
+	 * 
+	 * @return	A copy of the joint motion directions
+	 */
+	public int[] getJogMotion() {
+		return JOINT_MOTION.clone();
+	}
+	
+	public int getMotion(int mdx) {
+		if (mdx >= 0 && mdx < JOINT_MOTION.length) {
+			return JOINT_MOTION[mdx];
+		}
+		// Invalid motion index
+		return 0;
+	}
+	
+	public int setMotion(int mdx, int newDir) {
+		if (mdx >= 0 && mdx < JOINT_MOTION.length) {
+			int oldDir = JOINT_MOTION[mdx];
+			JOINT_MOTION[mdx] = newDir;
+			return oldDir;
+		}
+		// Invalid motion index
+		return 0;
+	}
+}
