408fe0824fb16313b86502549bc899dbc7905966
Joshua Hooker <jbhooker@mtu.edu>
2017-06-12 14:33:16 -0400

    More work on motion instructions

--- RobotRun/src/programming/CamMoveToObject.java
+++ RobotRun/src/programming/CamMoveToObject.java
@@ -4,8 +4,16 @@ import core.Scenario;
 import geom.Point;
 import geom.RMatrix;
 import geom.WorldObject;
+import global.Fields;
 import processing.core.PVector;
+import robot.RoboticArm;
 
+
+/**
+ * TODO general comments
+ * 
+ * @author Vincent Drucktes
+ */
 public class CamMoveToObject extends MotionInstruction {
 	
 	private Scenario scene;
@@ -20,7 +28,7 @@ public class CamMoveToObject extends MotionInstruction {
 	public CamMoveToObject(boolean isComm, int type, int WOdx, float spd,
 			int term, Scenario scene) {
 		
-		super(isComm, type, spd, term);
+		super(isComm, type, Fields.PTYPE_WO, WOdx, spd, term);
 		this.scene = scene;
 		
 		if (scene != null) {
@@ -31,17 +39,17 @@ public class CamMoveToObject extends MotionInstruction {
 		}
 	}
 	
-	private CamMoveToObject(boolean isComm, int mType, float spd, int term,
-			WorldObject tgtWO, Scenario scene) {
+	private CamMoveToObject(boolean isComm, int mType, int pType, int pdx,
+			float spd, int term, WorldObject tgtWO, Scenario scene) {
 		
-		super(isComm, mType, spd, term);
+		super(isComm, mType, pType, pdx, spd, term);
 		this.scene = scene;
 		this.tgtObj = tgtWO;
 	}
 	
 	public CamMoveToObject clone() {
-		return new CamMoveToObject(isCommented(), motionType, spdModifier,
-				termination, tgtObj, scene);
+		return new CamMoveToObject(isCommented(), motionType, posType, posIdx,
+				spdModifier, termination, tgtObj, scene);
 	}
 	
 	public Scenario getScene() {
@@ -53,26 +61,72 @@ public class CamMoveToObject extends MotionInstruction {
 	}
 	
 	public Point getWOPosition() {
-		WorldObject tgt = getTgtObject();
-		RMatrix tgtOri = tgt.getLocalOrientation();
-		PVector vecX = new PVector(tgtOri.getEntryF(0, 0), tgtOri.getEntryF(1, 0), tgtOri.getEntryF(2, 0));
-		PVector offset = vecX.mult(tgt.getForm().getDimArray()[0] / 2f + 5);
-					
-		Point pt = new Point(PVector.add(tgt.getLocalCenter(), offset), tgt.getLocalOrientation());
-		//float[] angles = RMath.inverseKinematics(this, getJointAngles(), pt.position, pt.orientation);
-		//pt.angles = angles;
+		if (tgtObj != null) {
+			WorldObject tgt = getTgtObject();
+			RMatrix tgtOri = tgt.getLocalOrientation();
+			PVector vecX = new PVector(tgtOri.getEntryF(0, 0), tgtOri.getEntryF(1, 0), tgtOri.getEntryF(2, 0));
+			PVector offset = vecX.mult(tgt.getForm().getDimArray()[0] / 2f + 5);
+						
+			Point pt = new Point(PVector.add(tgt.getLocalCenter(), offset), tgt.getLocalOrientation());
+			//float[] angles = RMath.inverseKinematics(this, getJointAngles(), pt.position, pt.orientation);
+			//pt.angles = angles;
+			
+			return pt;
+		}
 		
-		return pt;
+		return null;
 	}
 	
-	public void setWO(int WOdx) {
+	public void setPosIdx(int WOdx) {
+		posIdx = WOdx;
+		
 		if (scene != null) {
 			tgtObj = scene.getWorldObject(WOdx);
 		}
 	}
 	
+	@Override
 	public String[] toStringArray() {
-		// TODO
-		return new String[] { "TODO" };
+		int fieldLen = 5;
+		
+		String[] fields = new String[fieldLen];
+		
+		// Motion type symbol
+		int idx = 0;
+		fields[idx] = "\0";
+		
+		// Position type
+		++idx;
+		if (posType == Fields.PTYPE_WO) {
+			fields[idx] = "WO[";
+			
+		} else {
+			fields[idx] = "  [";
+		}
+		
+		// Position index
+		++idx;
+		if (tgtObj == null) {
+			fields[idx] = "...]";
+			
+		} else {
+			fields[idx] = String.format("%16s]", tgtObj.getName());
+		}
+		
+		// Motion speed modifier
+		++idx;
+		fields[idx] = String.format("%dmm/s",
+				Math.round(RoboticArm.motorSpeed * spdModifier));
+		
+		// Termination percent
+		++idx;
+		if (termination == 0) {
+			fields[idx] = "FINE";
+			
+		} else {
+			fields[idx] = String.format("CONT%d", termination);
+		}
+		
+		return fields;
 	}
 }
