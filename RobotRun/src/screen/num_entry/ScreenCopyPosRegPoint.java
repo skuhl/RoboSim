9a366ecfb90f92155b95bdbf4973d0a8d1783560
Vincent Druschke <vtdrusch@mtu.edu>
2017-06-21 16:49:22 -0400

    Implementing final number edit type screens

--- RobotRun/src/screen/num_entry/ScreenCopyPosRegPoint.java
+++ RobotRun/src/screen/num_entry/ScreenCopyPosRegPoint.java
@@ -2,6 +2,10 @@ package screen.num_entry;
 
 import core.RobotRun;
 import enums.ScreenMode;
+import global.DataManagement;
+import regs.PositionRegister;
+import regs.Register;
+import robot.RoboticArm;
 
 public class ScreenCopyPosRegPoint extends ST_ScreenNumEntry {
 
@@ -11,22 +15,44 @@ public class ScreenCopyPosRegPoint extends ST_ScreenNumEntry {
 
 	@Override
 	protected String loadHeader() {
-		return null;
+		Register reg = robotRun.getActiveRobot().getPReg(contents.getItemIdx());
+		return String.format("%s: POSITION COPY", reg.getLabel());
 	}
 	
 	@Override
 	protected void loadContents() {
-		
+		RoboticArm r = robotRun.getActiveRobot();
+		contents.setLines(robotRun.loadPositionRegisters(r));
 	}
 	
 	@Override
 	protected void loadOptions() {
-
+		options.addLine(String.format("Move PR[%d]'s point to:", contents.getItemIdx() + 1));
+		options.addLine(String.format("PR[%s]", workingText));
 	}
 
 	@Override
 	public void actionEntr() {
+		int regIdx = -1;
 
+		try {
+			// Copy the point of the curent Position register to the
+			// Position register at the specified index
+			regIdx = Integer.parseInt(workingText.toString()) - 1;
+			PositionRegister src = robotRun.getActiveRobot().getPReg(contents.getItemIdx());
+			PositionRegister dest = robotRun.getActiveRobot().getPReg(regIdx);
+			
+			dest.point = src.point.clone();
+			dest.isCartesian = src.isCartesian;
+			DataManagement.saveRobotData(robotRun.getActiveRobot(), 3);
+
+		} catch (NumberFormatException MFEx) {
+			System.err.println("Only real numbers are valid!");
+		} catch (IndexOutOfBoundsException IOOBEx) {
+			System.err.println("Only positve integers between 1 and 100 are valid!");
+		}
+
+		robotRun.lastScreen();
 	}
 
 }
